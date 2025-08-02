8a3ef07ceb8a630118df5fbc0423fe9d0f4e823f
Shivangi Kumari <shivangikumariwrs@gmail.com>
2025-08-03 03:14:00 +0530

    Initial commit - SecretBox project

---
+++ queries.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE secrets;
+
+\c secrets;
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(200) NOT NULL,
+  secret TEXT
+);
+
